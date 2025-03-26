8450ff5a1310bc66df113358f11d9661dfb70eb4
Li Xinran <1056133509@qq.com>
2025-03-26 22:50:08 +0800

    提交其他更改

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: '/NewsFocus/', // 部署到 GitHub Pages 时资源的基础路径
   plugins: [vue()],
   server: {
     open: true, // 启动时自动在浏览器打开
