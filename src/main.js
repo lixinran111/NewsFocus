6e3e52047ba12e7239e0ec01c9785502de88c34e
Li Xinran <1056133509@qq.com>
2025-03-26 20:59:14 +0800

    Deploy to GitHub Pages

--- src/main.js
+++
@@ -1,4 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue'; // 你的 Vue 根组件文件
-
-createApp(App).mount('#app');
--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    open: true, // 启动时自动在浏览器打开
-  },
-});
